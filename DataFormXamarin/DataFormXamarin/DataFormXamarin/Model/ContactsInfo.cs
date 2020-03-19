6cf07bfb036066c8326a07080424020af3789eb6
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-03-19 12:49:31 +0530

    XAMARIN-33999 - Sample added.

---
+++ DataFormXamarin/DataFormXamarin/DataFormXamarin/Model/ContactsInfo.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace DataFormXamarin
+{
+    public class ContactsInfo : INotifyPropertyChanged
+    {
+        private string firstName;
+        private string middleName;
+        private string lastName;
+        private string contactNo;
+        private string email;
+        private string address;
+        private DateTime? birthDate;
+        private string groupName;
+        public ContactsInfo()
+        {
+
+        }
+        public string FirstName
+        {
+            get { return this.firstName; }
+            set
+            {
+                this.firstName = value;
+                this.RaisePropertyChanged("FirstName");
+            }
+        }
+        public string MiddleName
+        {
+            get { return this.middleName; }
+            set
+            {
+                this.middleName = value;
+                this.RaisePropertyChanged("MiddleName");
+            }
+        }
+        public string LastName
+        {
+            get { return this.lastName; }
+            set
+            {
+                this.lastName = value;
+                this.RaisePropertyChanged("LastName");
+            }
+        }
+        public string ContactNumber
+        {
+            get { return contactNo; }
+            set
+            {
+                this.contactNo = value;
+                this.RaisePropertyChanged("ContactNumber");
+            }
+        }
+        public string Email
+        {
+            get { return email; }
+            set
+            {
+                email = value;
+                this.RaisePropertyChanged("Email");
+            }
+        }
+        public string Address
+        {
+            get { return address; }
+            set
+            {
+                address = value;
+                this.RaisePropertyChanged("Address");
+            }
+        }
+        public DateTime? BirthDate
+        {
+            get { return birthDate; }
+            set
+            {
+                birthDate = value;
+                this.RaisePropertyChanged("BirthDate");
+            }
+        }
+        public string GroupName
+        {
+            get { return groupName; }
+            set
+            {
+                groupName = value;
+                this.RaisePropertyChanged("GroupName");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged(String Name)
+        {
+            if (PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(Name));
+        }
+    }
+}
---
+++ DataFormXamarin/DataFormXamarin/DataFormXamarin/ViewModel/DataFormPageModel.cs
@@ -0,0 +1,27 @@
+﻿using DataFormXamarin;
+using FreshMvvm;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataFormXamarin
+{
+    public class DataFormPageModel : FreshBasePageModel
+    {
+        private ContactsInfo contactsInfo;
+        public ContactsInfo ContactsInfo
+        {
+            get { return this.contactsInfo; }
+            set
+            {
+                this.contactsInfo = value;
+                this.RaisePropertyChanged("ContactsInfo");
+            }
+        }
+
+        public DataFormPageModel()
+        {
+            this.contactsInfo = new ContactsInfo();
+        }
+    }
+}
